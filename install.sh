452ae16ce565d41a657fafc2fcbc4bb4b242230c
Neltarim <kyurann@gmail.com>
2021-03-14 13:56:22 +0100

    resolve new devices issues

--- install.sh
+++ install.sh
@@ -1,15 +1,1 @@
-sudo apt-get install git
-sudo apt-get install curl
-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone http://github.com/Neltarim/faster-zsh.git ~/Documents/faster-zsh
-cp ~/Documents/faster-zsh/code_aliases.sh ~/.oh-my-zsh/custom/
-source ~/.zshrc
-
-sudo pip3 install --upgrade setuptools
-sudo pip3 install --upgrade pip3
-sudo apt-get install libmysqlclient-dev
-sudo pip3 install mysql
-sudo pip3 install mysql-connector-python
-
-echo "Faster-zsh installed successfuly.\nTry \"finstall\" to install more usefuls programs."
+cp ~/Documents/faster-zsh/code_aliases.sh ~/.oh-my-zsh/custom/ && sudo pip3 install mysql && sudo pip3 install mysql-connector-python && echo "Faster-zsh installed successfuly.\nTry \"finstall\" to install more usefuls programs."
